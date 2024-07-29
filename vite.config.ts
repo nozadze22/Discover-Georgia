145a102593ef2aa9a718c87db09b801012963e2f
giorgi <127527335+nozadze22@users.noreply.github.com>
2024-07-29 14:42:30 +0400

    add pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins:[react()],
-    base: "{/project-georgiagit/}"
-
+    base: '/project-georgiagit/',
+    plugins:[react()]
+    
 })
