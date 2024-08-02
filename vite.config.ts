ef95be310e722f10c236fe937ee72510c69b252c
giorgi <127527335+nozadze22@users.noreply.github.com>
2024-08-02 12:16:26 +0400

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/project-georgia/',
+    base: '/discover-georgia/',
     plugins:[react()]
     
 })
